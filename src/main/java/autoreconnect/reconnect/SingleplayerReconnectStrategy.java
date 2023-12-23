a70371f5f41fdf8b366ea0928753c743e7d4eed6
TechPro424 <smsvra6@gmail.com>
2023-12-23 15:20:53 +0530

    An extremely half-baked fix for the reconnect override
    Needs to be tested
    
    The method signature for the IntegratedServerLoader.start() method was changed in either 1.20.3 or 1.20.4
    It now needs a runnable as the 2nd parameter (parameter name is onCancel)
    So as of now the onCancel runnable is just a lambda function as I am not sure what the runnable should do (maybe return the player to the title screen?)
    The runnable seems to be run whenever an exception is encountered while running the start() method and the world cannot be "recovered" (not too sure what that means)

--- src/main/java/autoreconnect/reconnect/SingleplayerReconnectStrategy.java
+++ src/main/java/autoreconnect/reconnect/SingleplayerReconnectStrategy.java
@@ -2,7 +2,6 @@ package autoreconnect.reconnect;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.MessageScreen;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.text.Text;
 
 public class SingleplayerReconnectStrategy extends ReconnectStrategy {
@@ -25,6 +24,13 @@ public class SingleplayerReconnectStrategy extends ReconnectStrategy {
         MinecraftClient client = MinecraftClient.getInstance();
         if (!client.getLevelStorage().levelExists(getName())) return;
         client.setScreenAndRender(new MessageScreen(Text.translatable("selectWorld.data_read")));
-        client.createIntegratedServerLoader().start(new TitleScreen(), getName());
+        client.createIntegratedServerLoader().start(getName(), () -> {});
+        /*
+         * The method signature for the IntegratedServerLoader.start() method was changed in either 1.20.3 or 1.20.4
+         * So as of now the onCancel runnable is just a lambda function as I am not sure what the runnable should do
+         * (maybe return the player to the title screen?)
+         * The runnable seems to be run whenever an exception is encountered while running the start() method and the world
+         * cannot be "recovered" (not too sure what that means)
+         */
     }
 }
