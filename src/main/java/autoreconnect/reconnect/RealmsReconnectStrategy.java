3ad12b43e2e45e5cc01ee9cbd6e24d195d697995
TechPro424 <smsvra6@gmail.com>
2024-02-14 20:34:38 +0530

    Make the reconnectMethod override use the RealmsPrepareConnectionTask, replicating the vanilla game Also removed the comments warning about the untested changes

--- src/main/java/autoreconnect/reconnect/RealmsReconnectStrategy.java
+++ src/main/java/autoreconnect/reconnect/RealmsReconnectStrategy.java
@@ -5,8 +5,7 @@ import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.realms.RealmsClient;
 import net.minecraft.client.realms.dto.RealmsServer;
 import net.minecraft.client.realms.gui.screen.RealmsLongRunningMcoTaskScreen;
-import net.minecraft.client.realms.gui.screen.RealmsMainScreen;
-import net.minecraft.client.realms.task.OpenServerTask;
+import net.minecraft.client.realms.task.RealmsPrepareConnectionTask;
 
 //import java.util.concurrent.locks.ReentrantLock;
 
@@ -28,14 +27,9 @@ public class RealmsReconnectStrategy extends ReconnectStrategy {
     @Override
     public void reconnect() {
         TitleScreen titleScreen = new TitleScreen();
-        OpenServerTask realmsGetServerDetailsTask = new OpenServerTask(realmsServer, new RealmsMainScreen(titleScreen), true, MinecraftClient.getInstance());
-        MinecraftClient.getInstance().setScreen(new RealmsLongRunningMcoTaskScreen(titleScreen, realmsGetServerDetailsTask));
-        /*
-         * The RealmsGetServerDetailsTask seems to have been removed in either 1.20.3 or 1.20.4
-         * The 2 Tasks that seemed to fit the most were OpenServerTask and RealmsPrepareConnectionTask
-         * I looked into the OpenServerTask, and saw that if the boolean join was true, 
-         * it would execute a play method in the RealmsMainScreen class that in turn runs the RealmsPrepareConnectionTask
-         * Hence I decided to use the OpenServerTask for now
-         */
+        RealmsPrepareConnectionTask realmsPrepareConnectionTask = new RealmsPrepareConnectionTask(titleScreen, realmsServer);
+
+        MinecraftClient.getInstance().setScreen(new RealmsLongRunningMcoTaskScreen(titleScreen, realmsPrepareConnectionTask));
+
     }
 }
--- src/main/java/autoreconnect/reconnect/SingleplayerReconnectStrategy.java
+++ src/main/java/autoreconnect/reconnect/SingleplayerReconnectStrategy.java
@@ -26,13 +26,5 @@ public class SingleplayerReconnectStrategy extends ReconnectStrategy {
         if (!client.getLevelStorage().levelExists(getName())) return;
         client.setScreenAndRender(new MessageScreen(Text.translatable("selectWorld.data_read")));
         client.createIntegratedServerLoader().start(getName(), () -> client.setScreen(new TitleScreen()));
-        /*
-         * The method signature for the IntegratedServerLoader.start() method was changed in either 1.20.3 or 1.20.4
-         * It now needs a runnable as the 2nd parameter (parameter name is onCancel)
-         * So as of now the onCancel runnable is just a lambda function as I am not sure what the runnable should do
-         * (maybe return the player to the title screen?)
-         * The runnable seems to be run whenever an exception is encountered while running the start() method and the world
-         * cannot be "recovered" (not too sure what that means)
-         */
     }
 }
